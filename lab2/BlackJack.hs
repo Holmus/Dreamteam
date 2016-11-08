10f2700b39cfc3f7d7bdd87f81d866939ac93963
Oscar Evertsson <oscar.evertsson@live.com>
2016-11-08 14:43:53 +0100

    Added description for the recursive call.

--- lab2/BlackJack.hs
+++ lab2/BlackJack.hs
@@ -3,6 +3,16 @@ module BlackJack where
 import Cards
 import RunGame
 
+{-
+size hand2
+  = size (Add (Card (Numeric 2) Hearts)
+              (Add (Card Jack Spades) Empty))
+  = 1 + size (Add (Card Jack Spades) Empty)
+  = 1 + 1 + size Empty
+  = 1 + 1 + 0
+  = 2
+-}
+
 
 -- Returns an empty hand
 empty :: Hand
