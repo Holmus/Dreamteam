59dc186d26ed58a7811b8da0cc34312c8a4817c4
Oscar Evertsson <oscar.evertsson@live.com>
2016-11-15 10:47:37 +0100

    Updated and removed old code.

--- lab3/sudoku.hs
+++ lab3/sudoku.hs
@@ -1,4 +1,4 @@
-
+import Data.Maybe
 
 
 data Sudoku = Sudoku { rows :: [[Maybe Int]] }
@@ -16,52 +16,12 @@ example =
     , [Nothing,Just 8, Just 3, Nothing,Nothing,Nothing,Nothing,Just 6, Nothing]
     , [Nothing,Nothing,Just 7, Just 6, Just 9, Nothing,Nothing,Just 4, Just 3]
     ]
-
+--A1
 allBlankSudoku :: Sudoku
 allBlankSudoku = Sudoku (replicate 9 (replicate 9 Nothing))
 
+
+
 isSudoku :: Sudoku -> Bool
-isSudoku s = rowSize == colSize && rowSize > 0
-        where rowSize = countRows s
-              colSize = countCols s
-
-
-
---Takes a sudoku and checks the amount of rows
-countRows :: Sudoku -> Int
-countRows s = length (rows s)
-                 
-
---Takes a soduku and checks the amount of columns in the first row
-countCols :: Sudoku -> Int
-countCols s | length (rows s) == 0 = 0
-countCols s = length (head (rows s))
-
--- Not part of lab only for fun
--- Takes a soduku and check if each column has the same 
-checkSameColSize :: Sudoku -> Bool
-checkSameColSize s | rows s == [] = True
-checkSameColSize s = checkSameColSizeRec s (countCols s)
-
--- Not part of lab only for fun
--- The recursive wrapper function which determines if 
--- the previous column length is the same as the current one
-checkSameColSizeRec :: Sudoku -> Int -> Bool
-checkSameColSizeRec s i | length (rows s) >= 1 =
-                          countCols s == i 
-                       && checkSameColSizeRec (Sudoku (drop 1 (rows s))) i
-                        | otherwise = True
-
-isEleNothing :: Maybe Int -> Bool
-isEleNothing m | m == Nothing = True
-               | otherwise = False
-
-
-isSolved :: Sudoku -> Bool
-isSolved s | length (rows s) == 0 = True
-isSolved s = all isEleNothing (head (rows s)) 
-             && isSolved (Sudoku (drop 1 (rows s)))
-
-
-
+isSudoku s = undefined
 
