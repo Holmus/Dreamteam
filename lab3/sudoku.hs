194d5ce528a2ecaa8ea60897778f407ee69e65ed
Oscar Evertsson <oscar.evertsson@live.com>
2016-11-15 11:34:36 +0100

    herp2

--- lab3/sudoku.hs
+++ lab3/sudoku.hs
@@ -4,6 +4,9 @@ import Data.Maybe
 data Sudoku = Sudoku { rows :: [[Maybe Int]] }
                 deriving Show
 
+ex1 = [Just 3, Just 9, Just 10, Nothing, Just 0]
+b = 
+
 example =
    Sudoku
     [ [Just 3, Just 6, Nothing,Nothing,Just 7, Just 1, Just 2, Nothing,Nothing]
@@ -17,11 +20,14 @@ example =
     , [Nothing,Nothing,Just 7, Just 6, Just 9, Nothing,Nothing,Just 4, Just 3]
     ]
 --A1
+--let a = [(Just x) | (Just x) <- ex1,  x < 10 && x > 0] ++ [y | y <- ex1, y == Nothing]
+(Just a) = a 
 allBlankSudoku :: Sudoku
 allBlankSudoku = Sudoku (replicate 9 (replicate 9 Nothing))
 
 
 
-isSudoku :: Sudoku -> Bool
+isSudoku :: Sudoku -> [[Maybe Int]]
 isSudoku s = undefined
+
 
