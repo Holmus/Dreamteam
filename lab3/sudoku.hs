2f1397ec837c46cfbf4a1fb43e54d13cc44126b9
Oscar Evertsson <oscar.evertsson@live.com>
2016-11-16 12:59:50 +0100

    Updated the isSoduku and created a mehtod 'isSolved.'

--- lab3/sudoku.hs
+++ lab3/sudoku.hs
@@ -1,11 +1,15 @@
 import Data.Maybe
 
 
+
 data Sudoku = Sudoku { rows :: [[Maybe Int]] }
                 deriving Show
 
 ex1 = [Just 3, Just 9, Just 10, Nothing, Just 0]
-b = 
+ex2 = Sudoku [[Just 3, Just 4, Just 5],
+              [Just 3, Just 4, Just 5],
+              [Just 3, Just 4, Nothing]  
+            ]
 
 example =
    Sudoku
@@ -17,17 +21,22 @@ example =
     , [Just 2, Just 7, Nothing,Just 4, Just 6, Nothing,Nothing,Nothing,Nothing]
     , [Nothing,Nothing,Just 5, Just 3, Nothing,Just 8, Just 9, Nothing,Nothing]
     , [Nothing,Just 8, Just 3, Nothing,Nothing,Nothing,Nothing,Just 6, Nothing]
-    , [Nothing,Nothing,Just 7, Just 6, Just 9, Nothing,Nothing,Just 4, Just 3]
+    , [Nothing,Nothing,Just 7, Just 6, Just 9, Nothing,Nothing,Just 4, Just 9]
     ]
 --A1
 --let a = [(Just x) | (Just x) <- ex1,  x < 10 && x > 0] ++ [y | y <- ex1, y == Nothing]
-(Just a) = a 
+
 allBlankSudoku :: Sudoku
 allBlankSudoku = Sudoku (replicate 9 (replicate 9 Nothing))
 
 
+isSudoku :: Sudoku -> Bool
+isSudoku s = length ([(Just x) | (Just x) <- oneArray, x < 10 && x > 0]
+                    ++ [y | y <- oneArray, y == Nothing])
+                    == 81 
+        where oneArray = concat (rows s)
 
-isSudoku :: Sudoku -> [[Maybe Int]]
-isSudoku s = undefined
-
+-- We here assume that it's a valid sudoku if you send it in. 
+isSolved :: Sudoku -> Bool
+isSolved s =  notElem Nothing (concat (rows s))
 
