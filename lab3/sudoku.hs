402bac731c3e5b28a2bf238e6be10e6a3f1e1990
Oscar Evertsson <oscar.evertsson@live.com>
2016-11-22 17:42:21 +0100

    Started workign on next lab and completed E1 and E2

--- lab3/sudoku.hs
+++ lab3/sudoku.hs
@@ -109,3 +109,30 @@ getSquare rows x y  = concat [ take 3 (drop x row) | row <- take 3 (drop y rows)
 
 isOkay :: Sudoku -> Bool
 isOkay s = and (map isOkayBlock (rows s))
+
+
+--E
+
+type Pos = (Int,Int)
+
+--E1
+
+blanks :: Sudoku -> [Pos]
+blanks s = [ (x,y)| x <- [0..8], y <- [0..8], isNothing ( ((rows s)!!x) !!y )]
+
+
+
+--E2
+--Also write (a) propert(y/ies) that state(s) the expected properties of this function. Think about what can go wrong! ????
+(!!=) :: [a] -> (Int,a) -> [a]
+(!!=) (x:xs) (i,el) | i < 0 = error "You can't replace a negative element in the list"
+                    | ((length (x:xs) - 1) < i) =
+                      error "You can't remove an element which is outside of the list"
+                    | i == 0 = el:xs
+                    | otherwise = x:((!!=) xs (i-1,el))
+
+
+--E3
+update :: Sudoku -> Pos -> Maybe Int -> Sudoku
+update s (x,y) el = undefined
+
