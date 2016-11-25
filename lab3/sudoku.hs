38ff88450285da8bb7d93038eb4e9b3fef711641
Oscar Evertsson <oscar.evertsson@live.com>
2016-11-25 19:54:18 +0100

    Fixed some properties and mistakes.

--- lab3/sudoku.hs
+++ lab3/sudoku.hs
@@ -14,7 +14,7 @@ ex2 = Sudoku [
               [Just 3, Just 4, Just 5]  
              ]
 example =
-   Sudoku
+  Sudoku
     [ [Just 3, Just 6, Nothing,Nothing,Just 7, Just 1, Just 2, Nothing,Nothing]
     , [Nothing,Just 5, Nothing,Nothing,Nothing,Nothing,Just 1, Just 8, Nothing]
     , [Nothing,Nothing,Just 9, Just 2, Nothing,Just 4, Just 7, Nothing,Nothing]
@@ -40,7 +40,7 @@ isSudoku s = length ([(Just x) | (Just x) <- oneArray, x < 10 && x > 0]
 -- For the sudoku to be solved it needs to be a legit sudoku, 
 -- then it can not contain any element of nothing. 
 isSolved :: Sudoku -> Bool
-isSolved s = isSolved s && notElem Nothing (concat (rows s))
+isSolved s = isSudoku s && notElem Nothing (concat (rows s))
 
 
 --B
@@ -95,7 +95,8 @@ isOkayBlock block | length block == 9 = length (nub numBlock) == length numBlock
 
 blocks :: Sudoku -> [Block]
 blocks s | isSudoku s = (getRows s) ++ (getColums s) ++ squares
-          where squares = [getSquare (rows s) x y | x <- [0,3,6], y <- [0,3,6]]
+         | otherwise  = [] 
+          where squares = [getSquare (rows s) x y | y <- [0,3,6], x <- [0,3,6]]
 
 getRows :: Sudoku -> [Block]
 getRows s = rows s
@@ -105,6 +106,10 @@ getColums s = transpose (rows s)
 
 getSquare :: [Block] -> Int -> Int -> Block
 getSquare rows x y  = concat [ take 3 (drop x row) | row <- take 3 (drop y rows)]
+
+prop_blocks_amountOfCells :: Sudoku -> Bool
+prop_blocks_amountOfCells (Sudoku rows) = length (blocks (Sudoku rows)) == 27
+                                        && all (\a -> if length a == 9 then True else False) rows
 
 isOkay :: Sudoku -> Bool
 isOkay s = and (map isOkayBlock (rows s))
@@ -119,14 +124,20 @@ type Pos = (Int,Int)
 blanks :: Sudoku -> [Pos]
 blanks s = [ (x,y)| x <- [0..8], y <- [0..8], isNothing ( ((rows s)!!x) !!y )]
 
+prop_blanks_allBlank :: Sudoku -> Bool
+prop_blanks_allBlank (Sudoku rows) = all (\(x,y) -> rows!!x!!y == Nothing) (blanks (Sudoku rows)) 
+
 --E2
 --Also write (a) propert(y/ies) that state(s) the expected properties of this function. Think about what can go wrong! ????
 (!!=) :: [a] -> (Int,a) -> [a]
-(!!=) (x:xs) (i,el) | i < 0 = error "You can't replace a negative element in the list"
-                    | (length (x:xs) - 1) < i =
-                      error "You can't remove an element which is outside of the list"
+(!!=) [] _          = [] 
+(!!=) (x:xs) (i,el) | i < 0 || (length (x:xs) - 1) < i =
+                      error "Invalid index"
                     | i == 0 = el:xs
                     | otherwise = x:((!!=) xs (i-1,el))
+
+
+--Write prop replace
 
 
 --E3
@@ -134,35 +145,33 @@ blanks s = [ (x,y)| x <- [0..8], y <- [0..8], isNothing ( ((rows s)!!x) !!y )]
 update :: Sudoku -> Pos -> Maybe Int -> Sudoku
 update (Sudoku rows) (row,col) el = Sudoku ((!!=) rows (row, ((!!=) (rows!!row) (col,el))))
 
+{-
+prop_update_correctVal :: Sudoku -> (Positive Int, Positive Int) -> Maybe Int -> Bool
+prop_update_correctVal s (row,col) el = (rows (update s (row,col) el))!!row!!col == el
+-}
 --E4
 
 candidates :: Sudoku -> Pos -> [Int]
-candidates sud (x,y) = intersect
+candidates sud (x,y) | isSudoku sud = intersect
                         (intersect (checkBlock row y) (checkBlock col x))
                         (checkBlock square sqPos) 
+                     | otherwise = []  
                        where block = blocks sud
                              sqPos = getSquarePos (x,y)
                              row   = block!!x
                              col   = block!!(9 + y)
-                             square= block!!(18 + sqPos)   
+                             square= block!!(18 + sqPos) 
+
+
 
 --Finds which block the x,y position belongs to. 
 getSquarePos :: Pos -> Int
 getSquarePos (x,y) = (x`div`3)*3 + y`div`3
 
 checkBlock :: [Maybe Int] -> Int -> [Int]
-checkBlock arr pos = [ y | y <- [0..8], isOkayBlock ((!!=) arr (pos,Just y))]
+checkBlock arr pos = [ y | y <- [1..9], isOkayBlock ((!!=) arr (pos,Just y))]
 
 -- F1
-solve :: Sudoku -> Maybe Sudoku
-solve s | not (isSudoku s && isOkay s) = Nothing
-        | otherwise = solve' s
-
-
-solve' :: Sudoku -> Maybe Sudoku
-solve' (Sudoku rows) = undefined
-                    
-
 
 
 
