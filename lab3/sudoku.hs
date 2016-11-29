05e4c878e99863c77a2299b1c8c5c15c1175744b
Jakob Holmgren <jholmgren95@gmail.com>
2016-11-29 19:13:08 +0100

    Commented all code and verified with lab pm

--- lab3/sudoku.hs
+++ lab3/sudoku.hs
@@ -28,7 +28,7 @@ example =
     , [Nothing,Nothing,Just 7, Just 6, Just 9, Nothing,Nothing,Just 4, Just 3]
     ]
 
--- Creates an empty sudoku board consisting of a 9x9 table "filled" with Nothing
+-- Creates an empty sudoku consisting of a 9x9 table "filled" with Nothing
 allBlankSudoku :: Sudoku
 allBlankSudoku = Sudoku (replicate 9 (replicate 9 Nothing))
 
@@ -43,7 +43,6 @@ isSudoku s = length ([(Just x) | (Just x) <- oneArray, x < 10 && x > 0]
 isSolved :: Sudoku -> Bool
 isSolved s = isSudoku s && notElem Nothing (concat (rows s))
 
-
 -- Given a maybe int, it returns it as a char. That is a Just 1-9, returns 1-9 and Nothing returns '.'
 printFormat :: Maybe Int -> Char
 printFormat (Just x) = intToDigit x
@@ -54,32 +53,33 @@ convertToString :: [[Char]] -> String
 convertToString (x:[]) = x ++ "\n"
 convertToString (x:xs) = x ++ "\n" ++ convertToString xs
 
--- ?
+-- Given a sudoku, creates instructions to print the sudoku on the screen, Nothing is represented by '.'
 printSudoku :: Sudoku -> IO ()
 printSudoku s | (rows s) == [] = return ()
               | otherwise = putStr (convertToString (map (map printFormat) (rows s)))
 
--- Given a filepath creates a sudoku from the string found in the file
+-- Given a filepath creates instructions that reads a sudoku from the file
 readSudoku :: FilePath -> IO Sudoku
 readSudoku path = do string <- readFile path
                      return (createSudoku string)
 
--- Given a char . or 1-9 returns it as a Maybe Int, i.e Just 1-9 or Nothing
+-- Given a char '.'' or an int 1-9 returns it as a Maybe Int, i.e Just 1-9 or Nothing
 charToMaybe :: Char -> Maybe Int
 charToMaybe c | c == '.' = Nothing
               | isDigit c = (Just (digitToInt c)) 
 
--- Given a string creates the corresponding sudoku if the string is using the desired notation (1-9, .)
+-- Given a string creates the corresponding sudoku if the string is using the desired notation (1-9, .).
+-- Utilizes charToMaybe
 createSudoku :: String -> Sudoku
 createSudoku s | isSudoku sudoku = sudoku
                | otherwise = error "The provided file is not a sudoku"
                where sudoku =  Sudoku ([[charToMaybe i | i <- x]| x <- words s])
 
--- Generates a cell, with 10% prob, of a Just int and 90% prob of Nothing. Utilizes randomJust
+-- Generates a cell, with 10% prob of a Just int and 90% prob of Nothing. Utilizes randomJust
 cell :: Gen (Maybe Int)
 cell =  frequency [(1,randomJust),(9,elements [Nothing])]
 
--- Generates a just int between just 1-9
+-- Generates a Just int between Just 1-9
 randomJust :: Gen (Maybe Int)
 randomJust = elements [Just i | i <- [1..9]]
 
@@ -88,14 +88,15 @@ instance Arbitrary Sudoku where
     do rows <- sequence [ sequence [ cell | j <- [1..9] ] | i <- [1..9] ]
        return (Sudoku rows)
 
--- Utilizes isSudoku to check whether the sudoku solely consists of allowed values
+-- Property for the isSudoku-function to check whether the sudoku solely consists of allowed values
 prop_Sudoku :: Sudoku -> Bool
 prop_Sudoku s = isSudoku s
 
--- Datatype consisting of an array of maybe ints, which we intend to use to represent one square, col or row of length 9
+-- Datatype consisting of an array of maybe ints, which is intended to be used to represent one square, col or row of length 9
 type Block = [Maybe Int]  
 
--- Verifies that the block provided is of length 9, consists of no Nothings and is solely unique values. If so returns true
+-- Verifies that the block provided is of length 9, consists of unique values with disregard to nothing.
+-- Returns true if those conditions are met
 isOkayBlock :: Block -> Bool
 isOkayBlock block | length block == 9 = length (nub numBlock) == length numBlock
                     where numBlock = filter (\a -> if a == Nothing then False else True) block
@@ -115,11 +116,11 @@ getRows s = rows s
 getColums :: Sudoku -> [Block]
 getColums s = transpose (rows s)
 
--- Given a sudoku and two indexes, returns a 3x3 square at the provided position
+-- Given a sudoku and two indexes, returns a 3x3 square at the provided position, where the indexes indicate the top-left corner.
 getSquare :: [Block] -> Int -> Int -> Block
 getSquare rows x y  = concat [ take 3 (drop x row) | row <- take 3 (drop y rows)]
 
--- Verifies that the number of cells are correct, that is, the blocks function should consist of 27x9 elements. 
+-- Verifies that the number of cells are correct, that is, the return value of the blocks function should consist of 27x9 elements. 
 prop_blocks_amountOfCells :: Sudoku -> Bool
 prop_blocks_amountOfCells (Sudoku rows) = length (blocks (Sudoku rows)) == 27
                                         && all (\a -> if length a == 9 then True else False) rows
@@ -150,7 +151,7 @@ prop_blanks_allBlank (Sudoku rows) = all (\(x,y) -> rows!!x!!y == Nothing) (blan
 -- E2
 -- Also write (a) propert(y/ies) that state(s) the expected properties of this function. Think about what can go wrong! ????
 
-
+-- Given a list and a tuple containing an index and a element, replace the element at the index with the elemeent provided.
 (!!=) :: [a] -> (Int,a) -> [a]
 (!!=) [] _          = [] 
 (!!=) (x:xs) (i,el) | i < 0 || (length (x:xs) - 1) < i =
@@ -166,10 +167,12 @@ rInt arr = do
               return x
 
 
+-- Property to verify that the length of the list is unchanged, that is, the element has been replaced, not solely the new one inserted nor the old one deleted.
 prop_replace_length' :: [a] -> a -> Property
 prop_replace_length' arr el = forAll (rInt arr) test
                               where test i = length arr == length (arr !!= (i,el))
 
+-- Property to verify that the element is inserted and that the rest of the list remains unchanged.
 prop_replace_correct' :: Eq a => [a] -> a -> Property  
 prop_replace_correct' [] el  = property ((!!=) [] (0,el) == [])  
 prop_replace_correct' arr el = forAll (rInt arr) test
@@ -177,22 +180,25 @@ prop_replace_correct' arr el = forAll (rInt arr) test
 
 --E3
 
--- 
+-- Function to update the provided sudoku at a provided position with a provided element, utilizes (!!=)
 update :: Sudoku -> Pos -> Maybe Int -> Sudoku
 update (Sudoku rows) (row,col) el = Sudoku ((!!=) rows (row, ((!!=) (rows!!row) (col,el))))
 
+-- Creates valid test input for prop_update_correctval.
 rPos :: Gen Pos
 rPos = do
           x <- elements [0..8]
           y <- elements [0..8]
           return (x,y)
-      
+
+-- Verifies that the element at the index provided is updated and correct.      
 prop_update_correctVal :: Sudoku -> Maybe Int -> Property
 prop_update_correctVal s el = forAll rPos update'
   where update' (x,y) = upArr(x,y)!!x!!y == el 
         upArr   (x,y) = (rows (update s (x,y) el))
 --E4
-  
+-- Returns all ints that could fill in the position provided in the sudoku provided.
+-- For values already filled in, returns a list consisting of the current one as well as possible replacements
 candidates :: Sudoku -> Pos -> [Int]
 candidates sud (x,y) | isSudoku sud = intersect
                         (intersect (checkBlock row y) (checkBlock col x))
@@ -204,12 +210,13 @@ candidates sud (x,y) | isSudoku sud = intersect
                              row   = block!!x
                              col   = block!!(9 + y)
                              square= block!!(18 + sqPos) 
-
+-- Creates valid test input for prop_candidates, it randomizes positions among those that are blank for the provided sudoku.
 rBlank :: Sudoku -> Gen Pos
 rBlank s = do
               (x,y) <- elements (blanks s)
               return (x,y)
 
+-- Checks whether the candidate functions findings are actual candidates.
 prop_candidates :: Sudoku -> Property
 prop_candidates sud = isOkay sud ==> forAll (rBlank sud) candidates'
   where allOkay arr     = and $ map isOkay arr
@@ -223,16 +230,17 @@ getSquarePos (x,y) = (x`div`3)*3 + y`div`3
 getArrPos :: Pos -> Int
 getArrPos (x,y) = x `mod` 3 * 3 + y `mod` 3
 
-
+-- For a provided list of maybe ints and an int checks where in the block that int could be inserted.
 checkBlock :: [Maybe Int] -> Int -> [Int]
 checkBlock arr pos = [ y | y <- [1..9], isOkayBlock ((!!=) arr (pos,Just y))]
 
 -- F1
-
+-- Checks whether the sudoku provided can actually be solved, then utlizes solve' to actually compute the solution.
 solve :: Sudoku -> Maybe Sudoku
 solve s | isSudoku s && isOkay s = solve' s (blanks s)
         | otherwise = Nothing
 
+-- Solves the sudoku provided by going over the blanks provided and trying out different candidates.
 solve' :: Sudoku -> [Pos] -> Maybe Sudoku
 solve' s [] = Just s        
 solve' s (fBlank:rBlank) = listToMaybe $ catMaybes 
@@ -240,7 +248,8 @@ solve' s (fBlank:rBlank) = listToMaybe $ catMaybes
                             [update s fBlank (Just cand) | cand <- candidates s fBlank]
                            ]  
         
--- F2        
+-- F2      
+-- Reads the file on the path provided and creates instructions to solve it and prints the answer.  
 readAndSolve :: FilePath -> IO ()
 readAndSolve path = do 
                         s <- readSudoku path
@@ -250,16 +259,18 @@ readAndSolve path = do
                         else printSudoku (fromJust (temp))
 
 -- F3
+-- Checks whether the 1st sudoku provided is solved, then by utilizing the helper-function if the 2nd sudoku is a "sub-sudoku" of the 1st.
 isSolutionOf :: Sudoku -> Sudoku -> Bool
 isSolutionOf s1 s2 = isSolved s1 && isSolutionOf' s1 s2
 
+-- Helper function which checks if the 2nd sudoku is a sub-sudoku of the first
 isSolutionOf' :: Sudoku -> Sudoku -> Bool
 isSolutionOf' (Sudoku s1) (Sudoku s2) = 
   all (\(x,y) -> if s1!!x!!y == s2!!x!!y then True else False) $
   [(x,y) | x <- [0..8], y <- [0..8], notElem (x,y) $ blanks (Sudoku s2)]
 
 -- F4 
-
+-- Property to verify that all sudokus solved are actually solved and valid by the original sudoku provided.
 prop_SolveSound :: Sudoku -> Property
 prop_SolveSound s = not (isNothing (solution)) ==>  property $ isSolved $
                     fromJust $ solution
