7d308a30b784f24359680e50bb971d81688e305b
Oscar Evertsson <oscar.evertsson@live.com>
2016-12-01 07:36:54 +0100

    Addressed all complaints....

--- lab3/sudoku.hs
+++ lab3/sudoku.hs
@@ -26,29 +26,27 @@ allBlankSudoku = Sudoku (replicate 9 (replicate 9 Nothing))
 
 --A2 Given a sudoku, verifies that it's a 9 by 9 sudoku. With only allowed values (Nothing or Just 1-9)
 isSudoku :: Sudoku -> Bool
-isSudoku s = length ([(Just x) | (Just x) <- oneArray, x < 10 && x > 0]
-                    ++ [y | y <- oneArray, y == Nothing])
-                    == 81 && length (getRows s) == 9
-             where oneArray = concat (rows s)
+isSudoku s = all (==9) (map length (rows s)) 
+          && all (==9) (map length (transpose (rows s))) 
+          && all legitVal (concat (rows s))
+          where legitVal (Just x) = if x > 0 && x < 10 then True else False
+                legitVal Nothing  = True 
+
+
 
 --A3 For the sudoku to be solved it needs to be a legit sudoku, that is, it can contain no "Nothing"-elements and has to consist of 1-9's solely.
 isSolved :: Sudoku -> Bool
-isSolved s = isSudoku s && notElem Nothing (concat (rows s))
+isSolved s = notElem Nothing (concat (rows s))
 
 -- Given a maybe int, it returns it as a char. That is a Just 1-9, returns 1-9 and Nothing returns '.'
 printFormat :: Maybe Int -> Char
 printFormat (Just x) = intToDigit x
 printFormat Nothing = '.'
 
--- Given an array of chars it combines them together as a string, separating each character with the new line
-convertToString :: [[Char]] -> String
-convertToString (x:[]) = x ++ "\n"
-convertToString (x:xs) = x ++ "\n" ++ convertToString xs
-
 --B1 Given a sudoku, creates instructions to print the sudoku on the screen, Nothing is represented by '.'
 printSudoku :: Sudoku -> IO ()
 printSudoku s | (rows s) == [] = return ()
-              | otherwise = putStr (convertToString (map (map printFormat) (rows s)))
+              | otherwise = putStr (unlines (map (map printFormat) (rows s)))
 
 --B2 Given a filepath creates instructions that reads a sudoku from the file
 readSudoku :: FilePath -> IO Sudoku
@@ -70,10 +68,7 @@ createSudoku s | isSudoku sudoku = sudoku
 --C1 Generates a cell, with 10% prob of a Just int and 90% prob of Nothing. Utilizes randomJust
 cell :: Gen (Maybe Int)
 cell =  frequency [(1,randomJust),(9,elements [Nothing])]
-
--- Generates a Just int between Just 1-9
-randomJust :: Gen (Maybe Int)
-randomJust = elements [Just i | i <- [1..9]]
+        where randomJust = elements [Just i | i <- [1..9]]
 
 
 --C2
@@ -91,23 +86,15 @@ type Block = [Maybe Int]
 --D1 Verifies that the block provided is of length 9, consists of unique values with disregard to nothing.
 -- Returns true if those conditions are met
 isOkayBlock :: Block -> Bool
-isOkayBlock block | length block == 9 = length (nub numBlock) == length numBlock
-  where numBlock = filter (\a -> if a == Nothing then False else True) block
+isOkayBlock block = length (nub numBlock) == length numBlock
+   where numBlock = filter isJust block
 
 --D2 Creates an array of blocks from a provided sudoku, if the sudoku consists of disallowed characters: returns [].
 -- Utilizes getRows, getColumns and getSquare
 blocks :: Sudoku -> [Block]
-blocks s | isSudoku s = (getRows s) ++ (getColums s) ++ squares
+blocks s | isSudoku s = (rows s) ++ (transpose (rows s)) ++ squares
          | otherwise  = [] 
           where squares = [getSquare (rows s) x y | y <- [0,3,6], x <- [0,3,6]]
-
--- Given a sudoku, returns all the rows as an array of blocks.
-getRows :: Sudoku -> [Block]
-getRows s = rows s
-
--- Given a sudoku, returns all the columns as an array of blocks.
-getColums :: Sudoku -> [Block]
-getColums s = transpose (rows s)
 
 -- Given a sudoku and two indexes, returns a 3x3 square at the provided position, where the indexes indicate the top-left corner.
 getSquare :: [Block] -> Int -> Int -> Block
@@ -120,7 +107,7 @@ prop_blocks_amountOfCells (Sudoku rows) = length (blocks (Sudoku rows)) == 27
 
 --D3 Checks through all rows and verifies that they are okay blocks, thus the whole sudoku is okay.
 isOkay :: Sudoku -> Bool
-isOkay s = and (map isOkayBlock (blocks s))
+isOkay s = all isOkayBlock (blocks s)
 
 
 --E, tuple of ints, represents a position.
