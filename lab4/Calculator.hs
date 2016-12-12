732241aa0171d4624c85632fcca7d897bebffacb
Jakob Holmgren <jholmgren95@gmail.com>
2016-12-12 11:19:53 +0100

    minor changes to make haste look a bit less cancerous

--- lab4/Calculator.hs
+++ lab4/Calculator.hs
@@ -44,6 +44,7 @@ main = do
     fx      <- mkHTML "<i>f</i>(<i>x</i>)="  -- The text "f(x)="
     input   <- mkInput 20 "x"                -- The formula input
     diff    <- mkButton "Draw differentiated graph"         -- The draw button
+    scaleT  <- mkHTML "scale="  -- The text "scale="
     zoom    <- mkInput 20 "1"
     scale   <- mkButton "Draw graph"         -- The draw button
       -- The markup "<i>...</i>" means that the text inside should be rendered
@@ -52,7 +53,11 @@ main = do
     -- Layout
     formula <- mkDiv
     row formula [fx,input]
-    column documentBody [canvas,formula,diff,zoom,scale]
+    szoom <- mkDiv
+    row szoom [scaleT, zoom]
+    buttons <- mkDiv
+    row buttons [scale, diff]
+    column documentBody [canvas,formula,szoom, buttons]
 
     -- Styling
     setStyle documentBody "backgroundColor" "lightblue"
