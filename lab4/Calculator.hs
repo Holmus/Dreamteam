75ec51777b5061c73657a8342530e3fcd0786a92
Jakob Holmgren <jholmgren95@gmail.com>
2016-12-12 11:13:53 +0100

    ugly piece of shit dick code fuck

--- lab4/Calculator.hs
+++ lab4/Calculator.hs
@@ -5,6 +5,7 @@ import Haste.DOM
 import Haste.Events
 import Haste.Graphics.Canvas
 import Data.Ix
+import Data.Char
 import Pages
 import Data.Maybe
 
@@ -15,24 +16,16 @@ import Expr
 canWidth  = 300
 canHeight = 300
 
-readAndDraw :: Elem -> Canvas -> IO ()
-readAndDraw e c = do s      <- getProp e "value"
-                     height <- getProp c "height"
-                     width  <- getProp c "width"
-                     let exp = fromJust (readExpr s)
-                     let ps = points exp ((read width)/canWidth) (read width, read height)
-                     render c (stroke $ path ps)
-
-readZoomAndDraw  :: Elem -> Canvas -> IO ()
-readZoomAndDraw e c = do s      <- getProp e "value"
-                         height <- getProp c "height"
-                         width  <- getProp c "width"
-                         scal   <- getProp e "zoom"
-                         let sca = eval (fromJust (readExpr scal)) 0
-                         let exp = fromJust (readExpr s)
-                         let ps = points exp ((read width)/canWidth) (read width, read height)
-                         render c (scale (sca,sca) (stroke $ path ps))
-
+readZoomAndDraw :: Elem -> Elem -> Canvas -> IO ()
+readZoomAndDraw z v c = do s    <- getProp v "value"
+                           height <- getProp c "height"
+                           width  <- getProp c "width"
+                           zoom   <- getProp z "value"
+                           let scale = eval (fromJust (readExpr zoom)) 0
+                           let exp = fromJust (readExpr s)
+                           let ps = points exp (1/scale) (read width, read height)
+                           render c (stroke $ path ps)
+ 
 points :: Expr -> Double -> (Int,Int) -> [Point]
 points e scale (w,h) = [formXY (i,eval e i) scale (w',h') | i <- [(-w')..w']]
     where w'  = realToFrac w
@@ -50,17 +43,16 @@ main = do
     canvas  <- mkCanvas (round canWidth) canHeight   -- The drawing area
     fx      <- mkHTML "<i>f</i>(<i>x</i>)="  -- The text "f(x)="
     input   <- mkInput 20 "x"                -- The formula input
-    draw    <- mkButton "Draw graph"         -- The draw button
     diff    <- mkButton "Draw differentiated graph"         -- The draw button
-    zoom    <- mkInput 20 "Zoom"
-    scale   <- mkButton "Scale"         -- The draw button
+    zoom    <- mkInput 20 "1"
+    scale   <- mkButton "Draw graph"         -- The draw button
       -- The markup "<i>...</i>" means that the text inside should be rendered
       -- in italics.
 
     -- Layout
     formula <- mkDiv
     row formula [fx,input]
-    column documentBody [canvas,formula,draw,zoom,scale]
+    column documentBody [canvas,formula,diff,zoom,scale]
 
     -- Styling
     setStyle documentBody "backgroundColor" "lightblue"
@@ -71,20 +63,19 @@ main = do
 
     -- Interaction
     Just can <- getCanvas canvas
-    onEvent draw  Click $ \_    -> readAndDraw input can
-    onEvent input KeyUp $ \code -> when (code==13) $ readAndDraw input can
-    onEvent diff  Click $ \_    -> readAndDrawDiff input can
-    onEvent scale Click $ \_    -> readZoomAndDraw zoom can
-    onEvent zoom KeyUp  $ \code -> when (code==13) $ readZoomAndDraw zoom can
+    onEvent diff  Click $ \_    -> readAndDrawDiff zoom input can
+    onEvent scale Click $ \_    -> readZoomAndDraw zoom input can
+    onEvent zoom  KeyUp $ \code -> when (code==13) $ readZoomAndDraw zoom input can
       -- "Enter" key has code 13
 
-readAndDrawDiff :: Elem -> Canvas -> IO ()
-readAndDrawDiff e c = do 
-                     s     <- getProp e "value"
-                     height<- getProp c "height"
-                     width <- getProp c "width"
-                     let exp = differentiate $ fromJust (readExpr s)
-                     _     <- setProp e "value" (showExpr exp)
-                     let ps = points exp ((read width)/canWidth) (read width, read height)
-                     render c (stroke $ path ps)
+readAndDrawDiff :: Elem -> Elem -> Canvas -> IO ()
+readAndDrawDiff z v c = do s    <- getProp v "value"
+                           height <- getProp c "height"
+                           width  <- getProp c "width"
+                           zoom   <- getProp z "value"
+                           let exp = differentiate $ fromJust (readExpr s)
+                           _ <- setProp v "value" (showExpr exp)
+                           let scale = eval (fromJust (readExpr zoom)) 0
+                           let ps = points exp (1/scale) (read width, read height)
+                           render c (stroke $ path ps)
 
